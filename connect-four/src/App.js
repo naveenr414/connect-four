7bdfbf74c483c538b3992366029bbd8a6d48953f
naveenr414 <dsfan414@gmail.com>
2018-10-08 23:41:00 -0400

    Created a borad

--- connect-four/src/App.js
+++ connect-four/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-		<h1> Hello </h1> 
+		<Board> </Board> 
 		
 		<Status> </Status> 
       </div>
@@ -20,6 +20,26 @@ class Status extends Component {
 			<div className="status"> 
 				Black is winning 
 			</div> 
+		);
+	}
+}
+
+class Board extends Component {
+	render() {
+		return (
+			<div> 
+				<Piece /> <Piece /> <Piece /> <br /> 
+				<Piece /> <Piece /> <Piece /> <br />
+				<Piece /> <Piece /> <Piece /> <br /> 
+			</div> 
+		);
+	}
+}
+
+class Piece extends Component{
+	render() {
+		return (
+			<span class="dot"></span>
 		);
 	}
 }
